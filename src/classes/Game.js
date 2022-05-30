4c9b48a9f20eaa38d2c86a46e5eb2e652ac4e7d5
Pavel Kinev <kinev.render@gmail.com>
2022-05-30 21:51:11 +0300

    catch tries logic

--- src/classes/Game.js
+++ src/classes/Game.js
@@ -4,15 +4,21 @@ import GameField from '../classes/GameField.js'
 class Game {
   #user = new User() // TODO: can be read from the local storage
   #field = new GameField()
+  #round = 1
+  #roundElement = document.getElementById('round')
+  #maxTries = 5
+  #tryElement = document.getElementById('tries')
   #tries = 5
 
   constructor() {
     this.#field.addHandlers(this.#onRodThrow.bind(this))
     this.#field.makePonds()
+
+    this.#gameRound = 1
+    this.#catchTries = this.#maxTries
   }
 
   #onRodThrow(hit) {
-    this.#tries -= 1
     const pond = this.#field.fishingInPond(hit)
     if (pond) this.#user.catchFish(pond)
     hit.remove(5_000)
@@ -21,11 +27,25 @@ class Game {
   }
 
   #checkRules() {
+    this.#catchTries = this.#tries - 1
+
     if (this.#tries === 0) {
       this.#field.removePonds()
-      this.#tries = 5
       this.#field.makePonds()
+
+      this.#gameRound = this.#round + 1
+      this.#catchTries = this.#maxTries
     }
+  }
+
+  set #gameRound(round) {
+    this.#round = round
+    this.#roundElement.innerText = this.#round
+  }
+
+  set #catchTries(tries) {
+    this.#tries = tries
+    this.#tryElement.innerText = Array.from({ length: tries }).map(() => '🪝').join('')
   }
 }
 
